969eb862ca989e953e2d19baa84e09fcd9769da7
Nikita Khlebnikov <khliebnikovmykyta@gmail.com>
2022-10-25 14:33:38 -0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This task done by Khliebnikov Nikita SE-19-1/9");
+
+    }
+}
